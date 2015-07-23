b02627425ea2a7fb45b29949e406459198a517fd
Adam Brown <adam@dicebag.com>
2015-07-23 14:16:23 -0500

    config unicorn.rb

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -4,25 +4,25 @@ else
 
 	# Set the working application directory
 	# working_directory "/path/to/your/app"
-	working_directory "/var/www/my_app"
+	working_directory "/var/www/adevotion"
 
 	# Unicorn PID file location
 	# pid "/path/to/pids/unicorn.pid"
-	pid "/var/www/my_app/pids/unicorn.pid"
+	pid "/var/www/adevotion/pids/unicorn.pid"
 
 	# Path to logs
 	# stderr_path "/path/to/log/unicorn.log"
 	# stdout_path "/path/to/log/unicorn.log"
-	stderr_path "/var/www/my_app/log/unicorn.log"
-	stdout_path "/var/www/my_app/log/unicorn.log"
+	stderr_path "/var/www/adevotion/log/unicorn.log"
+	stdout_path "/var/www/adevotion/log/unicorn.log"
 
 	# Unicorn socket
-	listen "/tmp/unicorn.[app name].sock"
+	listen "/tmp/unicorn.adevotion.sock"
 	listen "/tmp/unicorn.myapp.sock"
 
 	# Number of processes
 	# worker_processes 4
-	worker_processes 2
+	worker_processes 4
 
 	# Time-out
 	timeout 30
